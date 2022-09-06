46244ab2a578a80112592f150952314b2f2680b0
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-06 19:34:50 +0500

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: true, presence: { case_sensitive: false }       
   validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, length: { minimum: 3, maximum: 30  }
+  validates :password, presence: true, length: { in: Devise.password_length }, format: { with: /\A
+    (?=.{8,})      
+    (?=.*\d)         
+    (?=.*[a-z])      
+    (?=.*[A-Z])  
+    (?=.*[[:^alnum:]]) /x }, confirmation: true, on: :create 
 end
