895cc951d004ef5e64bb3dea3a9133bdb8761ed3
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-09 12:42:39 +0500

    remving unnecessory file

--- app/helpers/application_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# ApplicationHelpers
-module ApplicationHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'substances/index'
-  get 'substances/new'
-  get 'substances/create'
+  # get 'substances/index'
+  # get 'substances/new'
+  # get 'substances/create'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :substances
