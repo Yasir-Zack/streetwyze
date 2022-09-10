fb261c3598f7dee1f2913519991d9ac563881359
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-10 20:05:05 +0500

    remove substances controller

--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 2022_09_09_122824) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "substances", force: :cascade do |t|
-    t.string "place"
-    t.string "address"
-    t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
