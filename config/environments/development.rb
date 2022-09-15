08d483f7219f5d19ca644d54b27485926bf61f35
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-15 12:24:08 +0500

    Some code fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,6 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_options = { from: 'rahmanyasir64@gmail.com' }
