47157eb1c9bf79e6221dca3cca056ce47e549909
Yasir-Zack <rahmanyasir64@gmail.com>
2022-09-21 15:13:11 +0500

    survey model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_110416) do
+ActiveRecord::Schema.define(version: 2022_09_21_101050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2022_09_20_110416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
+  create_table "surveys", force: :cascade do |t|
+    t.string "survey_title"
+    t.string "survey_script"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
